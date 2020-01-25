ef3d3efca71288f03e9fb34f565c4102fbb904a4
AdrianMendezVargas <velociraptor088@gmail.com>
2020-01-25 13:02:46 -0400

    Solucionando Conexion con la Base de datos, Agregando Funciones: Limpiar, LlenarClasePersona, LlenaCampos, Programando Botones: Nuevo, guardar

--- DAL/Scripts/Scripts.sql
+++ DAL/Scripts/Scripts.sql
@@ -4,11 +4,12 @@ use PersonasDb
 go
 create table Personas
 (
-	Id int primary key identity,
+	PersonaID int primary key identity,
 	Nombre varchar(30),
 	Telefono varchar(15),
 	Cedula varchar(15),
-	Direccion varchar(max)
+	Direccion varchar(max),
+	FechaNacimiento dateTime
 
 
 );
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Registro.Entidades;
+﻿using Registro.BLL;
+using Registro.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +31,40 @@ namespace Registro {
         }
 
         private void btnGuardar_Click(object sender , RoutedEventArgs e) {
+
+            Persona persona;
+            bool guardado = true;
+
+            if (!CamposValidos()) {
+                return;
+            }
+
+            persona = LlenaClasePersona();
+
+            if(tbID.Text == "0") {
+
+                guardado = PersonasBLL.Guardar(persona);
+
+            } else {
+
+                if (!ExisteEnLaBaseDatos()) {
+                    MessageBox.Show("Este ID no se puede modificar porque no existe");
+                    tbID.Focus();
+                    return;
+                }
+                guardado = PersonasBLL.Modificar(persona);
+
+            }
+
+            if (guardado) {
+                MessageBox.Show("Guardado :)" , "EXITO" , MessageBoxButton.OK , MessageBoxImage.Exclamation);
+            } else {
+                MessageBox.Show("No se ah Guardado :(" , "ERROR" , MessageBoxButton.OK , MessageBoxImage.Error);
+            }
+
             
+            
+
 
 
         }
@@ -44,12 +78,14 @@ namespace Registro {
         }
 
         private void Limpiar() {
+
             tbID.Text = "";
             tbNombre.Text = "";
             tbTelefono.Text = "";
             tbCedula.Text = "";
             tbDireccion.Text = "";
-            tbFechaNacimiento.Text = "";
+            dpFechaNacimiento.SelectedDate = DateTime.Parse("01/01/1999");
+            
         }
 
         private Persona LlenaClasePersona() {
@@ -57,8 +93,78 @@ namespace Registro {
             Persona persona = new Persona();
 
             persona.PersonaID = Convert.ToInt32(tbID.Text);
+            persona.Nombre = tbNombre.Text;
+            persona.Cedula = tbCedula.Text;
+            persona.Telefono = tbTelefono.Text;
+            persona.Direccion = tbDireccion.Text;
+            persona.FechaNacimiento = (DateTime)dpFechaNacimiento.SelectedDate;
 
             return persona;
+        }
+
+        private void LlenaCamposInterfaz(Persona persona) {
+
+            tbID.Text = persona.PersonaID.ToString();
+            tbNombre.Text = persona.Nombre;
+            tbCedula.Text = persona.Cedula;
+            tbTelefono.Text = persona.Telefono;
+            tbDireccion.Text = persona.Direccion;
+            dpFechaNacimiento.SelectedDate = persona.FechaNacimiento;
+
+        }
+
+        private bool CamposValidos() {
+
+            bool validados = true;
+
+            int id;
+
+            try {
+
+                id = Convert.ToInt32(tbID.Text);
+
+            } catch (Exception) {
+
+                MessageBox.Show("El ID debe ser un numero");
+                tbID.Focus();
+                validados = false;
+
+            }
+
+            if (string.IsNullOrWhiteSpace(tbNombre.Text)) {
+                MessageBox.Show("El Nombre no puede estar vacio");
+                tbNombre.Focus();
+                validados = false;
+            }
+            if (string.IsNullOrWhiteSpace(tbCedula.Text)) {
+                MessageBox.Show("La cedula no puede estar vacia");
+                tbCedula.Focus();
+                validados = false;
+            }
+            if (string.IsNullOrWhiteSpace(tbTelefono.Text)) {
+                MessageBox.Show("El Telefono no puede estar vacio");
+                tbTelefono.Focus();
+                validados = false;
+            }
+            if (string.IsNullOrWhiteSpace(tbDireccion.Text)) {
+                MessageBox.Show("La direccion no puede estar vacia");
+                tbDireccion.Focus();
+                validados = false;
+            }
+            if (string.IsNullOrWhiteSpace(dpFechaNacimiento.SelectedDate.ToString()) ) {
+                MessageBox.Show("Seleccione una Fecha de Nacimiento");
+                dpFechaNacimiento.Focus();
+                validados = false;
+            }
+
+            return validados;
+        }
+
+        private bool ExisteEnLaBaseDatos() {
+
+            Persona persona = PersonasBLL.Buscar(Convert.ToInt32(tbID.Text));
+            return (persona != null);
+
         }
 
     }
