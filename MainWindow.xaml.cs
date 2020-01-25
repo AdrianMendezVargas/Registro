0f3de2d89283c525a389e9b821b702d29cbbc7f9
AdrianMendezVargas <velociraptor088@gmail.com>
2020-01-24 20:35:04 -0400

    Agregando : Terminando interfaz de registro; anadiendo imagenes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Registro.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,5 +22,44 @@ namespace Registro {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void btnNuevo_Click(object sender , RoutedEventArgs e) {
+
+            Limpiar();
+
+        }
+
+        private void btnGuardar_Click(object sender , RoutedEventArgs e) {
+            
+
+
+        }
+
+        private void btnEliminar_Click(object sender , RoutedEventArgs e) {
+
+        }
+
+        private void btnBuscar_Click(object sender , RoutedEventArgs e) {
+
+        }
+
+        private void Limpiar() {
+            tbID.Text = "";
+            tbNombre.Text = "";
+            tbTelefono.Text = "";
+            tbCedula.Text = "";
+            tbDireccion.Text = "";
+            tbFechaNacimiento.Text = "";
+        }
+
+        private Persona LlenaClasePersona() {
+
+            Persona persona = new Persona();
+
+            persona.PersonaID = Convert.ToInt32(tbID.Text);
+
+            return persona;
+        }
+
     }
 }
